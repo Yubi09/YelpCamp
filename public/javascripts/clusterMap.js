5f5534090d391733d6e0790f26ed29c81f87f2c0
Yubaraj <ydas6872@gmail.com>
2023-05-07 14:11:00 +0530

    my first commit

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,12 +1,13 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/dark-v11",
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
 
+map.addControl(new mapboxgl.NavigationControl(), "bottom-left");
 
 map.on("load", () => {
   // Add a new source from our GeoJSON data and
@@ -96,7 +97,7 @@ map.on("load", () => {
   map.on("click", "unclustered-point", (e) => {
     const { popUpMarkup } = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
-    
+
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
     // popup appears over the copy being pointed to.
@@ -104,10 +105,7 @@ map.on("load", () => {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(popUpMarkup)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
   });
 
   map.on("mouseenter", "clusters", () => {
